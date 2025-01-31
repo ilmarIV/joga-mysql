fc463d59ee4ddc74193271d1effda28d557177d7
VI <ilmar.vodi@voco.ee>
2025-01-31 11:47:55 +0200

    display all articles

--- controllers/articles.js
+++ controllers/articles.js
@@ -3,7 +3,9 @@ const db = require('../utils/db')
 const getAllArticles = (req, res) => {
     let sql = 'SELECT * FROM article'
     db.query(sql, (error, result) => {
-        console.log(result)
+        res.render('index', {
+            articles: result
+        })
     })
 }
 
